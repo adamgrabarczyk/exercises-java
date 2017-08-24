f002c4cd6bddd376226838ffbad20d5cfe18e772
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-24 20:43:21 +0200

    practise for loops - sum exercise

--- java-code/src/loops.java
+++ java-code/src/loops.java
@@ -113,7 +113,55 @@ public class loops {
       
       
       
-      
+        
+        
+        
+        
+        /*
+            1 2 3 4 5 6 7 8 9 10
+        
+            2 4 6 8 10 12 14 16 18 20
+        
+        */
+        
+        for (int j = 1; j <= 10; j++) // j =2
+        {
+            for (int i = 1; i <= 10; i++) // i = 5
+            {
+                
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
+//        
+//        for (int i = 0; i < 60; i++)
+//        {
+//            if (i % 2 != 0)
+//                continue; //przerwij iteracje od tego momentu i nie wykonuj już żadnych instrukcji po mnie
+//                
+//            System.out.println(i);
+//        }
+        
+        
+              System.out.println(" ");
+
+
+
+
+ int[] liczby = {1, 2, 3, 4, 5, 6};
+        
+        int suma = 0;
+        
+        for (int liczba: liczby)
+        {
+            suma += liczba;
+        }
+        
+        System.out.println(suma);
+
+
+
+
     }
     
 }
