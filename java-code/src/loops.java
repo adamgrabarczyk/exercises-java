e39ae9ccf9ecc26b78ca711efe6f0dfde00ba83a
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-24 20:32:57 +0200

    loops in another loops - multiplication table

--- java-code/src/loops.java
+++ java-code/src/loops.java
@@ -90,6 +90,29 @@ public class loops {
               */
         
       
+      // petla w petli
+      
+      
+      
+      
+       /*
+            1 2 3 4 5 6 7 8 9 10
+        
+            2 4 6 8 10 12 14 16 18 20
+        
+        */
+        
+        for (int j = 1; j <= 10; j++) // j = 3
+        {
+            for (int i = 1; i <= 10; i++) // i = 4
+            {
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
+      
+      
+      
       
     }
     
