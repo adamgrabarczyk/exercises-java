f64296da86d2e8a1098df456e5df11fa2d3b82c9
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-24 18:47:16 +0200

    fiew word's about operatores

---
+++ java-code/src/operators/operators.java
@@ -0,0 +1,56 @@
+package operators;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author adam
+ */
+public class operators {
+    
+    public static void main(String[] args) {
+        /*
+          OPERATOR - to znaki służące do operowania na zmiennych
+          
+           OPERATORY ARYTMETYCZNE
+               +   dodawanie liczb oraz łączenie string'ów
+               -   odejmowanie
+               *   mnożenie
+               /   dzielenie
+        
+               =   przypisanie wartości
+               +=  
+               -=
+               /=
+               *=
+               %=        
+               
+               %   reszta z dzielenia (dzielenie modulo)
+               
+
+               ++   inkrementacja - powiększ o 1
+               --   dekrementacja - zmniejsz o 1
+               
+               y++ - POST INKREMENTACJA
+               y-- - POST dekrementacja
+               
+               ++y - PRE INKREMENTACJA
+               --y - PRE dekrementacja               
+           
+         */
+        
+        int a = 5;
+        
+         // a += 1;
+        
+        System.out.println(a--);
+        System.out.println(a);
+        
+        System.out.println("daskasjdas");
+    }
+    
+}
