57554414ca28c83f194170686f710cbeea6b72bd
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-24 16:47:17 +0200

    initial netbeans ide

--- exercises/src/dziedziczenie/potowory/Zombie.java
+++
@@ -1,9 +0,0 @@
-package dziedziczenie.potowory;
-
-public class Zombie extends Potwor {
-
-	public Zombie() {
-		
-		System.out.println("to jest domyslny konstruktor z klasy zombie");
-	}
-}
--- exercises/src/pakunki/pakunki.java
+++
@@ -1,5 +0,0 @@
-package pakunki;
-
-public class pakunki {
-
-}
