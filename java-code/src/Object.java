e92464abaaad28334b8d1f6d9f21f70866232995
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-25 23:15:38 +0200

    obiect is a container with properties and method's

---
+++ java-code/src/Object.java
@@ -0,0 +1,30 @@
+
+public class Object {
+    
+      public static void main(String[] args) {
+      
+      screen abc = new screen();
+             abc.width = 124;
+     
+      screen abc2 = new screen();
+             abc2.width = 100;
+             
+             System.out.println(abc.width);
+             
+             System.out.println(abc2.width);
+             
+      }
+    
+     
+}
+
+
+ 
+      class screen {
+      
+          int width;
+          
+          int height;
+          
+          
+      }
