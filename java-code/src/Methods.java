96fd1105a837340b94109b7dc35877d618399f37
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-09 15:03:58 +0200

    simple methods which send parameters

---
+++ java-code/src/Methods.java
@@ -0,0 +1,31 @@
+
+public class Methods {
+    
+      public static void main(String[] args) {
+      
+     
+
+          
+ 
+
+test a = new test();
+
+
+a.print("Arek","Glizdogon");
+
+      }
+    
+     
+}
+
+
+class test {
+
+    void print (String name, String surname){
+   
+System.out.println(name + " " + surname);
+}
+
+}
+ 
+     
