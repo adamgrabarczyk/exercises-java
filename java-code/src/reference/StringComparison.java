8eee457aaebbc8871f91f29443e4229d1535e2c5
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-10 12:24:54 +0200

    comparison with '==' (not save)

---
+++ java-code/src/reference/StringComparison.java
@@ -0,0 +1,19 @@
+package reference;
+
+public class StringComparison {
+
+    public static void main(String[] args) {
+        
+        String name = "Adam";
+        
+        String name2 =  "Adam";
+        
+        
+        if (name == name2) {
+            System.out.println("Adam");
+        }
+        
+    }
+
+    
+}
