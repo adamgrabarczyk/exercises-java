affabbbab929155f4cf217219035e8a48791d147
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-10 13:12:01 +0200

    bootle code is done

--- java-code/src/bootle/Bootle.java
+++ java-code/src/bootle/Bootle.java
@@ -33,14 +33,28 @@ public class Bootle {
         
         }
         
-        void pastOut (double howMany) {
+        boolean pastOut (double howMany) {
         
-        this.howManyLiters += howMany;
+            if(howMany < howManyLiters)    
+        this.howManyLiters -= howMany;
+            else 
+                return false;
+            
+            return true;
         }
         
         
         
+        void pour (double howMany, Bootle wherePour){
         
+           if (this.pastOut(howMany))
+           {
+            wherePour.paste(howMany);
+        
+           }
+           else
+                System.out.println("za mala pojemnosc");
+        }
         
         
     public static void main(String[] args) {
@@ -51,8 +65,9 @@ public class Bootle {
         bootle[1] = new Bootle(8);
         bootle[2] = new Bootle(10);
     
-        bootle[0].paste(5);
+        bootle[0].paste(4);
         
+        bootle[0].pour(9, bootle[1]);
         
         System.out.println(bootle[0].getHowManyLiters());
         System.out.println(bootle[1].getHowManyLiters());
