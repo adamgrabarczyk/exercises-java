08dd3ed8e946e8b2e82c8e2c861088f81d01779c
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-20 19:26:29 +0200

    print student and employee descrition

--- java-code/src/instanceOff/instanceOff.java
+++ java-code/src/instanceOff/instanceOff.java
@@ -7,10 +7,11 @@ public class instanceOff {
  
        Person[] person = new Person[4];
         
-       person[0] = Employee();
+       person[0] = new Employee("Adam", "Grabarczyk", 65000);
        
-       person[1] = new ();
-       
+       person[1] = new Student("Mateusz", "Jakiś");
+      
+       person[1].downloadDescrition();
     }
 
     
@@ -22,19 +23,59 @@ abstract class Person {
 String name;
 
 String surname;
+
+   Person(String name, String surname) {
     
+    this.name = name;
+    
+    this.surname = surname;
+        
+    }
+    
+abstract void downloadDescrition();
+
 }
 
 
 class Employee extends Person{
 
 double salary;
+
+    public Employee(String name, String surname, double salary) {
+ 
+        super(name, surname);
+        
+        this.salary = salary;
+    }
+
+    @Override
+    void downloadDescrition() {
+        System.out.println("Jestem Pracownikiem");
+        System.out.println("moje imię to: " + name);
+        System.out.println("moje nazwisko to: " + surname);
+        System.out.println("moje wynagrodzenie to: " + salary);
+    }
     
+
+ 
+
+
 }
 
 
 class Student extends Person{
  
+   Student(String name, String surname) {
+    
+    super(name, surname);                      // wywołujemy wyżej położony konstruktor
+    }
+
+    @Override
+    void downloadDescrition() {
+        System.out.println("Jestem Studentem");
+        System.out.println("moje imię to: " + name);
+        System.out.println("moje nazwisko to: " + surname);
+    }
     
     
 }
