efd5b869b1c1fc847a3347324d69d8a33aef429d
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-25 20:00:44 +0200

    display date of start worl

--- java-code/src/finallKeyWord/finallKeyWord.java
+++ java-code/src/finallKeyWord/finallKeyWord.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package finallKeyWord;
 
-/**
- *
- * @author adam
- */
+import java.util.Date;
+
 public class finallKeyWord {
+
+    public static void main(String[] args) {
+        
+        final double PI = 3.14;  // stała tak jak const. nie można jej zmieniać
+        
+        Employee p = new Employee();
+        
+//        p.dataZatrudnienia = new Date(); to nie zadziała gdyz 'data zatrudnienia' jest fianl
+        
+        System.out.println(p.dataZatrudnienia);
+
+    }
     
+}
+
+
+
+abstract class Person {
+
+}
+
+class Employee extends Person {
+
+    final Date dataZatrudnienia = new Date();   // w tym przypadku trzeba zdefiniować odrazu zmienna
+
 }
